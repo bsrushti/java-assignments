57c13b16ce415f88856800b3da7b4346f1c11dae
bsrushti <bsrushti@in-bsrushti.local>
2019-03-12 10:07:38 +0530

    Implemented Console class.

---
+++ src/com/step/assignments/oop_part_1/polymorphism_excercise/Console.java
@@ -0,0 +1,8 @@
+package com.step.assignments.oop_part_1.polymorphism_excercise;
+
+public class Console {
+
+    public void displayContent(String content) {
+        System.out.println(content);
+    }
+}
